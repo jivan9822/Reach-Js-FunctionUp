f535956e925cd8d9b5371de434f97cc759328f16
jivan9822 <j.toshniwal@yahoo.co.in>
2023-01-03 14:04:28 +0530

    ass2 done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import logo from "../src/logo.png";
 import "../src/style.css";
+import { useState } from "react";
 function App() {
+  const [text, setText] = useState("");
+  const onChangeHandler = (e) => {
+    setText(e.target.value.toUpperCase());
+    console.log(text);
+  };
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    document.getElementsByClassName("input")[0].value = text;
+  };
   return (
-    <body>
-      <div className="container">
-        <div className="div-1">
-          <img src={logo} alt="logo" />
-          <span>Logo</span>
-        </div>
-        <input />
-        <div className="div-2">
-          <label>SearchButton</label>
-          <button>Google Search</button>
-          <button>i'm Feeling Lucky</button>
-          <label>LuckyButton</label>
-        </div>
-        <SmallComp title={"Francais"} />
-      </div>
-    </body>
-  );
-}
-
-function SmallComp({ title }) {
-  return (
-    <div className="div-3">
-      <h3>Google.ca offered in: {title}</h3>
-      <span>LanguageNote</span>
+    <div className="container">
+      <form>
+        <input
+          type="text"
+          className="input"
+          onChange={onChangeHandler}
+          name="name"
+          placeholder="Please type here"
+        />
+        <input
+          className="btn"
+          type="submit"
+          onClick={onClickHandler}
+          value="Submit"
+        />
+      </form>
     </div>
   );
 }
-
 export default App;
